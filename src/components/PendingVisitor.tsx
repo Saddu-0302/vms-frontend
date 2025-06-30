691a968f38837ec8f702b410affc5f77eb037b6a
mohammadsad <saadsayyad0302@gmail.com>
2025-06-30 11:22:19 +0530

    update pending visitor

--- src/components/PendingVisitor.tsx
+++ src/components/PendingVisitor.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import { useGetAllVisitorsQuery } from "../redux/visitorApi"
-import { swap } from "formik"
+
 import { UserCheck, UserX } from "lucide-react"
+import MenuList from "./MenuList"
 interface Visitor {
     _id: string
     name: string
@@ -128,6 +129,15 @@ const PendingVisitor = () => {
                         </div>
                     </div>)
                 }
+            </div>
+            <div className="drawer z-10">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                        <MenuList />
+                    </ul>
+                </div>
             </div>
         </div>
     </>
