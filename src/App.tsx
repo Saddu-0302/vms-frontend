43ca82b6f1c3737a752e03c5f875a4f7af8671e2
mohammadsad <saadsayyad0302@gmail.com>
2025-06-30 11:23:57 +0530

    added login route

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import Dashboard from './components/Dashboard'
 import CreateVisitor from './components/CreateVisitor'
 import AllVisitor from './components/AllVisitor'
 import PendingVisitor from './components/PendingVisitor'
+import Login from './components/Login'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <ToastContainer />
         <Routes>
+          <Route path='/' element={<Login />} />
           <Route path='/create-visitor' element={<CreateVisitor />} />
-          <Route path='/' element={<Dashboard />} />
+          <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/user-managment' element={<UserManagment />} />
           <Route path='/visitors' element={<AllVisitor />} />
           <Route path='/pending' element={<PendingVisitor />} />
