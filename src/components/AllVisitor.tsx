9c7ec20c8544207f014a91cc20256636c2da16e0
mohammadsad <saadsayyad0302@gmail.com>
2025-07-05 12:23:40 +0530

    change navbar style

--- src/components/AllVisitor.tsx
+++ src/components/AllVisitor.tsx
@@ -142,7 +142,7 @@ const AllVisitor = () => {
             {/* header */}
             <div className="navbar bg-base-100 shadow-sm ">
                 <div className="navbar-start">
-                    <div className="flex items-center gap-3">
+                    <div className="flex items-center gap-1">
                         <div className="text-sm text-gray-600">
                             <label htmlFor="my-drawer" className="cursor-pointer drawer-button rounded-full"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
                                 <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
